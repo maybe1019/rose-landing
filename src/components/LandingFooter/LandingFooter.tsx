8fcef9cc6ce0931da75ab522dd09f625be9d2c36
maybe1019 <maybex66@gmail.com>
2023-11-21 12:56:08 -0500

    add link to footer signup button

--- src/components/LandingFooter/LandingFooter.tsx
+++ src/components/LandingFooter/LandingFooter.tsx
@@ -10,9 +10,18 @@ const LandingFooter = () => {
           src="/static/img/new_logo/png/RoseAIlogo-gradient.png"
           alt="logo"
         />
-        <div className="text-[32px] md:text-[36px] font-bold text-center">Let’s get started on something great.</div>
-        <div className="text-[20px] md:text-[24px] text-center">Join over 4,000+ startups already growing with Untitled.</div>
-        <Link to={"/"} className="no-underline !text-white bg-primary rounded-3xl py-2 px-10">Sign up</Link>
+        <div className="text-[32px] md:text-[36px] font-bold text-center">
+          Let’s get started on something great.
+        </div>
+        <div className="text-[20px] md:text-[24px] text-center">
+          Join over 4,000+ startups already growing with Untitled.
+        </div>
+        <Link
+          to={"/register"}
+          className="no-underline !text-white bg-primary rounded-3xl py-2 px-10"
+        >
+          Sign up
+        </Link>
       </div>
       <div className="flex justify-between py-16 flex-col md:flex-row gap-8">
         <div>© 2023 Rose AI. All rights reserved.</div>
